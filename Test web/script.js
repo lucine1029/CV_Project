251a76458ecd342a5de2cb6231bdafa0387c34ae
lucine1029 <sherry.jing@hotmail.com>
2024-02-27 19:15:59 +0100

    Added EasterEggs and print-style.css

---
+++ Test web/script.js
@@ -0,0 +1,101 @@
+
+// It works well with changeable backgroundcolor
+/*Easter egg one*/ 
+
+const titleElement = document.getElementById("title");
+
+const backGroundColor = [
+    'lightblue',
+    'lightyellow',
+    '#ffb6c1',
+    '#8fbc8f',
+    'rgb(231, 229, 229)'
+];
+
+let imageIndex = 0;
+
+titleElement.addEventListener('click', function() {
+    document.querySelector('.main-content').style.backgroundColor = backGroundColor[imageIndex];
+    imageIndex = (imageIndex + 1) % backGroundColor.length;
+});
+
+
+/*Easter egg two*/ 
+
+const surpriseModule = document.getElementById('surpriseModule');
+
+const closeIcon = document.getElementById('close-icon');
+
+let input = [];
+window.addEventListener('keyup', function(e) {
+    input.push(e.key);
+
+    if (input.length === 4) {
+        const sequence = input.join('');
+        if (sequence === '0331') {
+            showModule();
+        }
+        input.length = 0;
+    }
+});
+
+function showModule() {
+    surpriseModule.style.display = 'block';
+};
+
+closeIcon.addEventListener('click', function () {
+    surpriseModule.style.display = 'none';
+});
+
+
+
+/* try 1*/
+
+
+
+// const backGroundImage = 'url(images/EasterEgg/Firework.png)';
+
+// titleElement.addEventListener('click', function() {
+//     document.querySelector('.main-content').style.backgroundImage = backGroundImage;
+// });
+
+
+
+
+
+/* try 2*/
+
+// const backgroundImage = [
+//     'url(images/EasterEgg/Firework.png)',
+//     'url(images/EasterEgg/EasterEgg1.png)',
+//     'url(images/EasterEgg/EasterEgg2.png)'
+// ];
+
+// let imageIndex = 0;
+
+// titleElement.addEventListener('click', function() {
+//     document.body.style.backgroundImage = backgroundImage[imageIndex];
+
+//     imageIndex = (imageIndex + 1) % backgroundImage.length;
+
+// });
+
+
+
+
+
+
+
+
+// const header = document.querySelector("#header");
+// header.style.color = "blue";
+
+// const title = document.getElementById("title");
+// title.style.color = "green";
+
+
+
+
+
+// const background = document.querySelector("div");
+// background.style.backgroundColor = "orange";
